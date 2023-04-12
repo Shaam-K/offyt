4b20c364fd527d13b74bf3e25118a858943848a6
Shaam <arkshaam@gmail.com>
2023-04-12 23:49:14 +0530

    added more information about how many videos it was able to fetch and how many failed to do so, made the site more mobile responsive

--- offyt_web/offyt_app/views.py
+++ offyt_web/offyt_app/views.py
@@ -22,7 +22,11 @@ def playlist_view(request):
     if request.method == "GET":
         playlist_video_info = []
         playlist_links = []
-        search = request.GET['url']
+        search = request.GET['id']
+
+
+        status = []
+
 
         playlist = Playlist(f'https://www.youtube.com/playlist?list={search}')
 
@@ -34,9 +38,7 @@ def playlist_view(request):
             playlist_links.append(links)
 
 
-        if len(playlist_links) == 0:
-            print("enter valid playlist")
-
+        exception_count = 0
 
         for item in playlist_links:
 
@@ -47,15 +49,32 @@ def playlist_view(request):
                     'author': item_yt.author,
                     'title': item_yt.title,
                     'thumb_url': item_yt.thumbnail_url,
+                    'link': item,
                 }
 
 
                 playlist_video_info.append(video_details)
 
             except:
+                exception_count += 1
                 pass
+
+        generated_links = len(playlist_links) - exception_count
+
+
+        video_status = []
+
+        playlist_schema = {
+            'playlist_name ': str(directory),
+            'exception_check': exception_count,
+            'total_generated': generated_links
+        }
+
+        video_status.append(playlist_schema)
+
+        print(video_status)
             
 
-    context = {'playlist_details': playlist_video_info}
+    context = {'playlist_details': playlist_video_info, 'generated_videos': video_status}
     
     return render(request, 'base.html', context)
