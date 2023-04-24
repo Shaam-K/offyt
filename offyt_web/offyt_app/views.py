08eff184bbb8591209ea0fda06b330849cb1fb73
Shaam <arkshaam@gmail.com>
2023-04-25 00:24:30 +0530

    now metadata is inserted into audiofiles and also thumbnails are scraped for them also

--- offyt_web/offyt_app/views.py
+++ offyt_web/offyt_app/views.py
@@ -8,6 +8,8 @@ import os,music_tag
 
 import requests
 
+import glob
+
 
 
 playlist_video_info = []
@@ -69,30 +71,24 @@ def playlist_download(request):
 
     if request.method == "GET":
 
+        audio_info = []
+
         video_download = request.GET.getlist('check_vid')
+
+        video_count = len(video_download)
 
 
         videos_downloaded = 0
 
-        path = 'offyt_app/static/audiofiles'
+        audio_path = r'offyt_app\static\audiofiles'
+
+        images_path = r'offyt_app\static\imagefiles'
 
 
-        while videos_downloaded < len(video_download):
+        while videos_downloaded < video_count:
 
             try:
                 video_obj = YouTube(video_download[videos_downloaded])
-
-
-                video_filter = video_obj.streams.filter(only_audio=True, file_extension="webm")
-                
-                video_filter.first().download(path)
-
-
-                path_files = os.listdir(path)
-
-
-                print(path_files)
-
 
                 video_details = {
                     'author': video_obj.author,
@@ -100,31 +96,70 @@ def playlist_download(request):
                     'thumb_url': video_obj.thumbnail_url,
                 }
 
+            
+                audio_info.append(video_details)
 
 
-                print(video_details)
-
-
-
-                ''' 
+                video_filter = video_obj.streams.filter(only_audio=True, file_extension="mp4")
                 
-                audiofile = music_tag.load_file(path_files[videos_downloaded])
+                video_filter.first().download(audio_path)
 
-                audiofile['title'] = video_details['title']
-                audiofile['artist'] = video_details['author']
 
-                audiofile.save()
-                
-                '''
 
-               
-
-    
                 videos_downloaded += 1
-
             except:
                 pass
+
+
+    
+        # adding metadata to audiofiles
+
+        meta_pointer = 0
+
+        os.makedirs(images_path)
         
+        # Getting the list of files/directories
+        # in the specified path Filtering the 
+        # list to exclude the directory names
+        files = list(filter(os.path.isfile, glob.glob(audio_path + "/*")))
+
+        files.sort(key=os.path.getctime)
+        
+        
+        print(files)
+
+
+        print(audio_info)
+
+
+
+        for file in files:
+
+            image_title = audio_info[meta_pointer]['title']
+
+            url = audio_info[meta_pointer]['thumb_url']
+
+
+            image_data = requests.get(url).content
+            
+
+            image_file = open(f'{images_path}\{meta_pointer}.jpg', 'wb')
+
+            image_file.write(image_data)
+
+            image_file.close
+            audiofile = music_tag.load_file(file)
+            audiofile['title'] = image_title
+            audiofile['artist'] = audio_info[meta_pointer]['author']
+            audiofile.save()
+
+            meta_pointer += 1
+
+
+
+    
+
+                        
     context = {}
 
     return render(request, 'download.html', context)
